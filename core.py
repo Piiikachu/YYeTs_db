b16d71c1ac3a9327e23205cfcbbc1ba71bca4bbe
wanglichi <wanglichi@cmhi.chinamobile.com>
2021-02-19 10:53:30 +0800

    Update core.py

--- core.py
+++ core.py
@@ -1,15 +1,33 @@
 import json
-from os import path
-
-CONFIG_PATH = 'config.json'
+import os
 
 
-def getDBPath() -> str:
-    if path.exists(CONFIG_PATH):
-        with open(CONFIG_PATH, 'r', encoding='UTF-8') as f:
-            config: dict = json.load(f)
-            if 'dbPath' in config.keys():
-                dbPath = config['dbPath']
-                print(f'成功读取yyets.db路径: {dbPath}')
-                return dbPath
-    return None
+class AppCore():
+    def __init__(self) -> None:
+        self.CONFIG_PATH = 'config.json'
+        self.config = self.getConfig()
+
+    def getConfig(self) -> dict:
+        if os.path.exists(self.CONFIG_PATH):
+            with open(self.CONFIG_PATH, 'r', encoding='UTF-8') as f:
+                config: dict = json.load(f)
+                return config
+        return {}
+
+    def setConfig(self, config: dict):
+        with open(self.CONFIG_PATH, 'w', encoding='UTF-8') as f:
+            json.dump(config, f, indent=4)
+
+    def getDBPath(self) -> str:
+        if 'dbPath' in self.config.keys():
+            return self.config['dbPath']
+        return None
+
+    def setDBPath(self, path: str):
+        self.config['dbPath'] = path
+        self.setConfig(self.config)
+
+
+if __name__ == '__main__':
+    core = AppCore()
+    print(core.getConfig())
